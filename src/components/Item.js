dce5708b9c08c0ee8648c2a3fff3410714e7f293
Jack Pena <penajackc@gmail.com>
2022-02-02 08:23:05 -0700

    completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [add, setAdd] = useState(false)
+
+  const adjustClass = add ? "in-cart" : ""
+
+  function handleAddClick(){
+    setAdd(() => !add)
+  }
+
   return (
-    <li className="">
+    <li className={adjustClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddClick} className="add">{add ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
